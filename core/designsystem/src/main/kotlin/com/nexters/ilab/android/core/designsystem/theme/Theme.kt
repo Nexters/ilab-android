08662f143dab0a3fb3b3448ffd82819b92ed4a85
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-01-28 15:56:05 +0900

    [DESIGN] Theme statusBar, navigationBar 색상 변경

--- core/designsystem/src/main/kotlin/com/nexters/ilab/android/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/nexters/ilab/android/core/designsystem/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -53,11 +54,24 @@ fun ILabTheme(
         else -> LightColorScheme
     }
     val view = LocalView.current
+
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+
+            window.statusBarColor = if (darkTheme) Color.Black.toArgb() else Color.White.toArgb()
+            window.navigationBarColor = if (darkTheme) Color.Black.toArgb() else Color.White.toArgb()
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                // remove unnecessary black screen from bars
+                window.isNavigationBarContrastEnforced = false
+            }
+
+            val windowsInsetsController = WindowCompat.getInsetsController(window, view)
+
+            // status bar's icon always visible
+            windowsInsetsController.isAppearanceLightStatusBars = !darkTheme
+            windowsInsetsController.isAppearanceLightNavigationBars = !darkTheme
         }
     }
 
