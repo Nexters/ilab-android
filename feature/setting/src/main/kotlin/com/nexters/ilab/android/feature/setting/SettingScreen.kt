b7d4a54b76ad6f0daf1b4946fe651718057603f8
josu_shell <yoy07030@khu.ac.kr>
2024-02-08 00:57:44 +0900

    [CHORE][#27] SettingScreen.kt DeteKt 수정

--- feature/setting/src/main/kotlin/com/nexters/ilab/android/feature/setting/SettingScreen.kt
+++ feature/setting/src/main/kotlin/com/nexters/ilab/android/feature/setting/SettingScreen.kt
@@ -12,10 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -104,7 +102,7 @@ internal fun SettingContent(onLogoutClick: () -> Unit, onDeleteAccountClick: () 
             text = stringResource(id = R.string.setting_delete_account),
             style = Contents1,
             color = Color.Black,
-            modifier = Modifier.clickable(onClick = onDeleteAccountClick)
+            modifier = Modifier.clickable(onClick = onDeleteAccountClick),
         )
     }
 }
