0e09f682a63853b3a6c494960988a459a98360eb
josushell <yoy07030@khu.ac.kr>
2024-02-11 22:20:56 +0900

    [CHORE][#13] MainNavController.kt detekt 수정

--- feature/main/src/main/kotlin/com/nexters/ilab/android/feature/main/MainNavController.kt
+++ feature/main/src/main/kotlin/com/nexters/ilab/android/feature/main/MainNavController.kt
@@ -17,7 +17,6 @@ import com.nexters.ilab.android.feature.mypage.navigation.navigateToMyPage
 import com.nexters.ilab.android.feature.setting.navigation.navigateToSetting
 import com.nexters.ilab.android.feature.uploadphoto.navigation.navigateToCreateImage
 import com.nexters.ilab.android.feature.uploadphoto.navigation.navigateToCreateImageComplete
-import com.nexters.ilab.android.feature.uploadphoto.navigation.navigateToInputKeyword
 import com.nexters.ilab.android.feature.mypage.navigation.navigateToMyAlbumImage
 import com.nexters.ilab.android.feature.uploadphoto.navigation.navigateToInputStyle
 
