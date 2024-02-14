b0a9c3fb0d366e78ba2b662ad21ac8d453dd3173
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-02-14 17:03:48 +0900

    [FEAT] [#16] 스플래시 화면 UI 구현

--- feature/intro/src/main/kotlin/com/nexters/ilab/android/feature/intro/IntroScreen.kt
+++ feature/intro/src/main/kotlin/com/nexters/ilab/android/feature/intro/IntroScreen.kt
@@ -1,14 +1,26 @@
 package com.nexters.ilab.android.feature.intro
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.nexters.ilab.android.core.designsystem.R
+import com.nexters.ilab.core.ui.component.BackgroundImage
+import kotlinx.coroutines.delay
 
 @Suppress("unused")
 @Composable
@@ -18,6 +30,7 @@ internal fun IntroRoute(
     viewModel: IntroViewModel = hiltViewModel(),
 ) {
     LaunchedEffect(key1 = Unit) {
+        delay(1000)
         navigateToLogin()
     }
 
@@ -26,11 +39,36 @@ internal fun IntroRoute(
 
 @Composable
 internal fun IntroScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        BackgroundImage(
+            resId = R.drawable.bg_splash_screen,
+            contentDescription = "Background Image for Splash Screen",
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+        )
+        IntroContent(
+            modifier = Modifier.align(Alignment.Center),
+        )
+    }
+}
+
+@Composable
+fun IntroContent(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(text = "SplashScreen")
+        Image(
+            painter = painterResource(id = R.drawable.ic_splash),
+            contentDescription = "Splash Image",
+            contentScale = ContentScale.Fit,
+        )
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_ilab_logo_64),
+            contentDescription = "iLab Logo",
+            tint = Color.Unspecified,
+        )
     }
 }
