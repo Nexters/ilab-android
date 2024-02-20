6728006a889c60a3107a825f6320c406b3efc3cc
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-02-21 00:58:31 +0900

    [FEAT] SharedViewModel 확장 함수 common 모듈로 이동
    공통 함수를 사용하는 방식으로 변경

--- feature/createimage/src/main/kotlin/com/nexters/ilab/feature/createimage/navigation/CreateImageNavigation.kt
+++ feature/createimage/src/main/kotlin/com/nexters/ilab/feature/createimage/navigation/CreateImageNavigation.kt
@@ -1,15 +1,11 @@
 package com.nexters.ilab.feature.createimage.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import com.nexters.ilab.android.core.common.extension.sharedViewModel
 import com.nexters.ilab.feature.createimage.CreateImageCompleteRoute
 import com.nexters.ilab.feature.createimage.CreateImageRoute
 import com.nexters.ilab.feature.createimage.viewmodel.CreateImageViewModel
@@ -52,16 +48,4 @@ fun NavGraphBuilder.createImageNavGraph(
             )
         }
     }
-}
-
-// https://youtu.be/h61Wqy3qcKg?si=OqctoATR5MGbypOW
-@Composable
-inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(
-    navController: NavHostController,
-): T {
-    val navGraphRoute = destination.parent?.route ?: return hiltViewModel()
-    val parentEntry = remember(this) {
-        navController.getBackStackEntry(navGraphRoute)
-    }
-    return hiltViewModel(parentEntry)
 }
--- feature/uploadphoto/src/main/kotlin/com/nexters/ilab/android/feature/uploadphoto/navigation/UploadPhotoNavigation.kt
+++ feature/uploadphoto/src/main/kotlin/com/nexters/ilab/android/feature/uploadphoto/navigation/UploadPhotoNavigation.kt
@@ -1,16 +1,12 @@
 package com.nexters.ilab.android.feature.uploadphoto.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import com.nexters.ilab.android.core.common.extension.sharedViewModel
 import com.nexters.ilab.android.feature.uploadphoto.InputStyleRoute
 import com.nexters.ilab.android.feature.uploadphoto.UploadCheckRoute
 import com.nexters.ilab.android.feature.uploadphoto.UploadPhotoRoute
@@ -74,16 +70,4 @@ fun NavGraphBuilder.uploadPhotoNavGraph(
             )
         }
     }
-}
-
-// https://youtu.be/h61Wqy3qcKg?si=OqctoATR5MGbypOW
-@Composable
-inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(
-    navController: NavHostController,
-): T {
-    val navGraphRoute = destination.parent?.route ?: return hiltViewModel()
-    val parentEntry = remember(this) {
-        navController.getBackStackEntry(navGraphRoute)
-    }
-    return hiltViewModel(parentEntry)
 }
