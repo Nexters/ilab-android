638264c244f8fef61805aeead7e0812b94ae52fb
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-02-27 23:03:30 +0900

    [CHORE] [#88] style check success

--- feature/intro/src/main/kotlin/com/nexters/ilab/android/feature/intro/viewmodel/IntroSideEffect.kt
+++ feature/intro/src/main/kotlin/com/nexters/ilab/android/feature/intro/viewmodel/IntroSideEffect.kt
@@ -3,5 +3,4 @@ package com.nexters.ilab.android.feature.intro.viewmodel
 sealed interface IntroSideEffect {
     data object AutoLoginSuccess : IntroSideEffect
     data object AutoLoginFail : IntroSideEffect
-
 }
